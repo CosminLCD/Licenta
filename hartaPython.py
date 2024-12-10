d89f8443a05fc3856f6103d4fd6c24b12da7d07e
Cosmin Libotean <liboteancosmin@gmail.com>
2024-12-10 20:12:24 +0200

    Incercare

--- hartaPython.py
+++ hartaPython.py
@@ -8,7 +8,7 @@ getLoc = loc.geocode("Cismigiu, Romania")
 lat_cluj= 46.772166
 long_cluj= 23.583807
 
-lat_coruia= 47.56480680803573
+lat_coruia= 47.56480680803573 # cordonate
 long_coruia=23.60368731749156
 
 
